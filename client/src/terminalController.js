dd73870a8997fa5be1c78bbbca7a01453979e637
Drinnn <pedro_lontro@hotmail.com>
2021-03-26 17:42:27 -0300

    feat: add functions on enter and on message received

--- client/src/terminalController.js
+++ client/src/terminalController.js
@@ -1,7 +1,40 @@
+import ComponentBuilder from "./components.js"
 export default class TerminalController {
     constructor () {}
 
+    #onInputReceived (eventEmitter) {
+        return function () {
+            const message = this.getValue()
+            console.log(message)
+            this.clearValue()
+        }
+    }
+
+    #onMessageReceived ({ screen, chat }) {
+        return msg => {
+            const { username, message } = msg 
+
+            chat.addItem(`{bold}${username}{/}: ${message}`)
+
+            screen.render()
+        }
+    }
+
+    #registerEvents (eventEmitter, components) {
+        eventEmitter.on('message:received', this.#onMessageReceived(components))
+    }
+
     async initializeTable (eventEmitter) {
-        console.log("Initialized!")
+        const components = new ComponentBuilder()
+            .setScreen({ title: 'KoolHackerChat'})
+            .setLayoutComponent()
+            .setInputComponent(this.#onInputReceived(eventEmitter))
+            .setChatComponent()
+            .build()
+
+        this.#registerEvents(eventEmitter, components)
+
+        components.input.focus()
+        components.screen.render()
     }
 }
