cc2739105ad85da05fb822d06a08ce656e10ebff
Drinnn <pedro_lontro@hotmail.com>
2021-03-31 09:48:10 -0300

    feat: add socket controller

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import Event from 'events'
 import { constants } from './constants.js'
+import Controller from './controller.js'
 import SocketServer from "./socket.js" 
 
 const eventEmitter = new Event()
@@ -33,6 +34,9 @@ const port = process.env.PORT || 9898
 const socketServer = new SocketServer({ port })
 const server = await socketServer.initialize(eventEmitter)
 console.log(`socket server is running at ${server.address().port}`)
+
+const controller = new Controller({ socketServer })
+eventEmitter.on(constants.event.NEW_USER_CONNECTED, controller.onNewConnection.bind(controller))
 
 // eventEmitter.on(constants.event.NEW_USER_CONNECTED, socket => {
 //     console.log('new connection!', socket.id)
